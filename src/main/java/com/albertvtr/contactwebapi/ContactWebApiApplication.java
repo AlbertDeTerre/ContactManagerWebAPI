275da7971bfb87511b8aeb6e86e57f73a626c2cd
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-26 13:21:12 +0200

    Create Spring Boot project

---
+++ src/main/java/com/albertvtr/contactwebapi/ContactWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.albertvtr.contactwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactWebApiApplication.class, args);
+    }
+    
+}
