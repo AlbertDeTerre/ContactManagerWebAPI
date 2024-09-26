7ddfdab743eb24a0dc490b792b26887c021cc1cf
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-26 15:22:42 +0200

    Create Company class

---
+++ src/main/java/com/albertvtr/contactwebapi/model/Company.java
@@ -0,0 +1,42 @@
+package com.albertvtr.contactwebapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numVAT;
+    private Address address;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "company_contact",
+            joinColumns = @JoinColumn(name = "company_id"),
+            inverseJoinColumns = @JoinColumn(name = "contact_id")
+    )
+    private List<Contact> contacts;
+
+    public Company() {}
+    public Company(String numVAT, Address address) {
+        this.numVAT = numVAT;
+        this.address = address;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
