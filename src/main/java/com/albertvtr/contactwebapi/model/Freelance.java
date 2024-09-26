68e406b57e377fd94d710e569f77f23905ae22a6
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-26 14:50:02 +0200

    Create Freelance class

---
+++ src/main/java/com/albertvtr/contactwebapi/model/Freelance.java
@@ -0,0 +1,24 @@
+package com.albertvtr.contactwebapi.model;
+
+public class Freelance extends Contact {
+
+    private String numVAT;
+
+    public Freelance(){
+        super();
+        setContactType(ContactType.FREELANCE);
+    }
+
+    public Freelance(String firstName, String lastName, Address address, String numVAT) {
+        super(firstName, lastName, address, ContactType.FREELANCE);
+        setNumVAT(numVAT);
+    }
+
+    public String getNumVAT() {
+        return numVAT;
+    }
+
+    public void setNumVAT(String numVAT) {
+        this.numVAT = numVAT;
+    }
+}
