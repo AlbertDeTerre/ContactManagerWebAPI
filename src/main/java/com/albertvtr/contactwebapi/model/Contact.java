196ae58125232b5002921902925b7c1518ba9b99
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-26 14:33:57 +0200

    Create Contact class

---
+++ src/main/java/com/albertvtr/contactwebapi/model/Contact.java
@@ -0,0 +1,83 @@
+package com.albertvtr.contactwebapi.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "contact_type", discriminatorType = DiscriminatorType.STRING)
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private ContactType contactType;
+
+    public Contact() {}
+
+    public Contact(String firstName, String lastName, Address address, ContactType contactType) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAddress(address);
+        setContactType(contactType);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public ContactType getContactType() {
+        return contactType;
+    }
+
+    public void setContactType(ContactType contactType) {
+        this.contactType = contactType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
