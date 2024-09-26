2a2de24421bdc6dadcfa7be42cef14e2080cc92b
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-26 13:39:23 +0200

    Create Address class

---
+++ src/main/java/com/albertvtr/contactwebapi/model/Address.java
@@ -0,0 +1,9 @@
+package com.albertvtr.contactwebapi.model;
+
+public class Address {
+    private String street;
+    private String number;
+    private String city;
+    private String postCode;
+    private String country;
+}
