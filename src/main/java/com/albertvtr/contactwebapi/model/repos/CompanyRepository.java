0a528799bcf3915b1d675d8e3defbba0685b1247
Albert de terre Voiturier <albertdeterrevoiturier@Alberts-MacBook-Pro.local>
2024-09-29 18:10:34 +0200

    Update CompanyRespoitory to add findByNumVAT method

--- src/main/java/com/albertvtr/contactwebapi/model/repos/CompanyRepository.java
+++ src/main/java/com/albertvtr/contactwebapi/model/repos/CompanyRepository.java
@@ -3,5 +3,8 @@ package com.albertvtr.contactwebapi.model.repos;
 import com.albertvtr.contactwebapi.model.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByNumVAT(String numVAT);
 }
